a80450716d49b36aa457c4c59de6ecbba32f73f3
DSojat <145054801+DSojat@users.noreply.github.com>
2024-02-25 20:03:05 +0000

    Deploying to gh-pages from @ DSojat/goit-js-hw-10@27e9b89edf49247283d8bd0e5a6f64caa6f16240 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,17 +1,2 @@
-import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                      */import{f as d,i as l}from"./assets/vendor-77e16229.js";const u=document.querySelector("#datetime-picker"),a=document.querySelector("button[data-start]"),p=document.querySelector(".timer");a.disabled=!0;let n,t;const m={enableTime:!0,time_24hr:!0,defaultDate:new Date,minuteIncrement:1,onClose(e){n=e[0].getTime(),n<Date.now()?l.show({message:"Please choose a date in the future",messageColor:"white",messageSize:"18",theme:"dark",backgroundColor:"red",position:"topCenter",close:!1,progressBar:!1,iconUrl:"./img/circle.svg"}):a.disabled=!1}};d(u,m);a.addEventListener("click",f);function f(){t=n-Date.now(),a.disabled=!0;const e=setInterval(()=>{t=t-1e3,t<1e3&&(clearInterval(e),t=0);const s=h(t);v(s),p.innerHTML=`<div class="field">
-          <span class="value" data-days>${s.days}</span>
-          <span class="label">Days</span>
-        </div>
-        <div class="field">
-          <span class="value" data-hours>${s.hours}</span>
-          <span class="label">Hours</span>
-        </div>
-        <div class="field">
-          <span class="value" data-minutes>${s.minutes}</span>
-          <span class="label">Minutes</span>
-        </div>
-        <div class="field">
-          <span class="value" data-seconds>${s.seconds}</span>
-          <span class="label">Seconds</span>
-        </div>`},1e3)}function h(e){const o=Math.floor(e/864e5),r=Math.floor(e%864e5/36e5),i=Math.floor(e%864e5%36e5/6e4),c=Math.floor(e%864e5%36e5%6e4/1e3);return{days:o,hours:r,minutes:i,seconds:c}}function v(e){for(const s in e)e[s]=e[s].toString().padStart(2,"0")}
+import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                      */import{f as l,i as m}from"./assets/vendor-77e16229.js";const s=document.querySelector("#datetime-picker"),a=document.querySelector("button[data-start]"),h=document.querySelector(".value[data-days]"),f=document.querySelector(".value[data-hours]"),y=document.querySelector(".value[data-minutes]"),b=document.querySelector(".value[data-seconds]");a.disabled=!0;let r,e;const p={enableTime:!0,time_24hr:!0,defaultDate:new Date,minuteIncrement:1,onClose(t){r=t[0].getTime(),r<Date.now()?m.show({message:"Please choose a date in the future",messageColor:"white",messageSize:"18",theme:"dark",backgroundColor:"red",position:"topCenter",close:!1,progressBar:!1,iconUrl:"./img/circle.svg"}):a.disabled=!1}};l(s,p);a.addEventListener("click",v);function v(){s.setAttribute("disabled","true"),e=r-Date.now(),a.disabled=!0;const t=setInterval(()=>{e=e-1e3,e<1e3&&(clearInterval(t),e=0,s.removeAttribute("disabled"));const o=S(e);h.textContent=n(o.days),f.textContent=n(o.hours),y.textContent=n(o.minutes),b.textContent=n(o.seconds)},1e3)}function S(t){const d=Math.floor(t/864e5),u=Math.floor(t%864e5/36e5),c=Math.floor(t%864e5%36e5/6e4),i=Math.floor(t%864e5%36e5%6e4/1e3);return{days:d,hours:u,minutes:c,seconds:i}}function n(t){return t.toString().padStart(2,"0")}
 //# sourceMappingURL=commonHelpers.js.map
